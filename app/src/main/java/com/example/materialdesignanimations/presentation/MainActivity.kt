0162654ebea908d1cea13851fd53dc7f780ab9a8
casensio <cristina.asensio.munoz@gmail.com>
2019-07-11 20:53:13 +0200

    Shared Elements Transition implemented

--- app/src/main/java/com/example/materialdesignanimations/presentation/MainActivity.kt
+++ app/src/main/java/com/example/materialdesignanimations/presentation/MainActivity.kt
@@ -4,6 +4,8 @@ import android.app.ActivityOptions
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Pair
+import android.view.View
 import com.example.materialdesignanimations.AnimationType
 import com.example.materialdesignanimations.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -22,12 +24,22 @@ class MainActivity : AppCompatActivity() {
         buttonFadeCode.setOnClickListener { goToActivity(AnimationType.FADE_CODE) }
         buttonSlideXml.setOnClickListener { goToActivity(AnimationType.SLIDE_XML) }
         buttonSlideCode.setOnClickListener { goToActivity(AnimationType.SLIDE_CODE) }
+        sharedElements.setOnClickListener { goToSharedElementsActivity() }
     }
 
     private fun goToActivity(animationType: AnimationType) {
         val options: ActivityOptions = ActivityOptions.makeSceneTransitionAnimation(this)
         val intent = Intent(this, AnimationActivity::class.java)
         intent.putExtra(animation_type, animationType)
+        startActivity(intent, options.toBundle())
+    }
+
+    private fun goToSharedElementsActivity() {
+        val pairTitle = Pair<View, String>(sharedElementsTv, "sharedElementsTitle")
+        val pairImage = Pair<View, String>(sharedElementsIv, "sharedElementsImage")
+
+        val options = ActivityOptions.makeSceneTransitionAnimation(this, pairTitle, pairImage)
+        val intent = Intent(this, SharedElementsActivity::class.java)
         startActivity(intent, options.toBundle())
     }
 }
---
+++ app/src/main/java/com/example/materialdesignanimations/presentation/SharedElementsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.materialdesignanimations.presentation
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.materialdesignanimations.R
+
+class SharedElementsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shared_elements)
+    }
+}
